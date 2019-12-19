74ef1e194bacd1200ad1b9d391a847b522a14e4b
Jessica Triana <jessica.triana.tx@gmail.com>
2019-12-19 21:38:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,7 +110,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     programmer_hash[:alan_kay][:known_for][0] = "GUI"
+     programmer_hash[:alan_kay][:known_for][0] = alans_new_info
+     programmer_hash[:alan_kay][:known_for][0]
 end
 
 def adding_to_dennis
